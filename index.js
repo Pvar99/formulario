bde54ae31789ee8a6c77e3ea605cdf6e03501c9f
Pvar99 <vargaspaul99@gmail.com>
2022-05-31 15:28:18 -0600

    i made changes

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-var guardar = function () {
+var Datospersonales = [];
+var arrayconvertido = []
+var leerinformacion = function () {
 
-    var dato = document.getElementById('lista').value
-    console.log(dato)
+
+    var arrayconvertido = JSON.parse(localStorage.getItem('Datospersonales'))
+    Datospersonales = arrayconvertido;
+    console.log(arrayconvertido)
+
+    document.getElementById('resultados').innerHTML = ''
+    for (let a = 0; a < arrayconvertido.length; a++) {
+
+
+        document.getElementById('resultados').innerHTML += '<tr><td>' + arrayconvertido[a].nombre + '</td><td>' + arrayconvertido[a].apellido + '</td> <td> ' + arrayconvertido[a].direccion + '</td><td class="btn btn-danger"  onclick="eliminar(' + a + ')">quitar</td></td>'
+
+    }
 }
+
+var guardar = function () {
+    var nombre = document.getElementById('nombre').value
+    var apellido = document.getElementById('apellido').value
+    var direccion = document.getElementById('direccion').value
+    Datospersonales.push({ nombre: nombre, apellido: apellido, direccion: direccion })
+    localStorage.setItem('Datospersonales', JSON.stringify(Datospersonales))
+    leerinformacion()
+
+}
+
+
+
+
+var eliminar = function (indice) {
+    console.log(indice)
+    Datospersonales.splice(indice, 1)
+    localStorage.setItem('Datospersonales', JSON.stringify(Datospersonales))
+    leerinformacion()
+}
+leerinformacion()
